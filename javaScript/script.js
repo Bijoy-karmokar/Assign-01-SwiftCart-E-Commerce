3c4c7d04b9633a326e6926f68699b5df5f093e1d
Bijoy-karmokar <bijoykarmokar320@gmail.com>
2026-02-17 10:20:53 +0600

    active btn

--- javaScript/script.js
+++ javaScript/script.js
@@ -10,9 +10,14 @@ const displayCategories = (categories) => {
     // Create ALL button first
     const allBtn = document.createElement("button");
     allBtn.innerText = "All";
-    allBtn.classList.add("btn", "rounded-3xl");
+    allBtn.classList.add("btn", "rounded-3xl","category-btn");
 
     allBtn.addEventListener("click", () => {
+          document.querySelectorAll(".category-btn")
+            .forEach(btn => btn.classList.remove("active"));
+
+        // Add active to clicked button
+        allBtn.classList.add("active");
         loadAllProducts();
     });
 
@@ -21,9 +26,15 @@ const displayCategories = (categories) => {
     categories.forEach(category => {
         const button = document.createElement("button");
         button.innerText = category;
-        button.classList.add("btn", "rounded-3xl");
-
+        button.classList.add("btn", "rounded-3xl","category-btn");
+        
         button.addEventListener("click", () => {
+             // Remove active from all buttons
+        document.querySelectorAll(".category-btn")
+            .forEach(btn => btn.classList.remove("active"));
+
+        // Add active to clicked button
+        button.classList.add("active");
             loadProductCate(category);
         });
 
@@ -87,7 +98,43 @@ const loadProductCate =async(category)=>{
 }
 
 const displayProducts=(products)=>{
-    console.log(products);
+    const allCategory = document.getElementById('all-category');
+    allCategory.innerHTML=""
+    products.forEach(product=>{
+        // console.log(product);
+        
+        const createDiv = document.createElement('div');
+        createDiv.innerHTML=`
+          <div class="card bg-base-100 shadow-md">
+            <figure class="bg-gray-200">
+              <img
+                src=${product.image}
+                class="w-44 h-50 p-4"
+                alt="Shoes"
+              />
+            </figure>
+            <div class="card-body">
+              <div class="flex justify-between items-center font-semibold gap-16 w-full">
+                <div class="badge badge-soft badge-primary">${product.category}</div>
+                <p>${product.rating.rate} (${product.rating.count})</p>
+              </div>
+              <p class="truncate font-semibold"> 
+                ${product.title}
+              </p>
+              <p class="font-semibold">$ ${product.price}</p>
+              <div class="card-actions justify-between">
+                <div class="btn px-5">
+                  <i class="fa-regular fa-eye"></i> Details
+                </div>
+                <div class="btn btn-primary px-5">
+                  <i class="fa-solid fa-cart-shopping"></i> Add
+                </div>
+              </div>
+            </div>
+          </div>
+        `
+        allCategory.append(createDiv);
+    })
     
 }
 
